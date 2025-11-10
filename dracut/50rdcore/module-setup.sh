23159c3c64e0d33ad1bde42041311edc175423fb
Bipin B Narayan <bbnaraya@redhat.com>
2025-11-10 23:41:16 +0530

    Add rdcore dracut module

---
+++ dracut/50rdcore/module-setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+install() {
+    local _arch=${DRACUT_ARCH:-$(uname -m)}
+
+    inst_simple "$moddir/rdcore" "/usr/bin/rdcore"
+
+    # `rdcore kargs` calls `zipl` on s390x
+    if [[ "$_arch" == "s390x" ]]; then
+        inst_multiple zipl
+        inst /lib/s390-tools/stage3.bin
+        inst /lib/s390-tools/zipl_helper.device-mapper
+    fi
+}
