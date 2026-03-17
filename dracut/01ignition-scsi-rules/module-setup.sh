e67d6fa12a514e11939389dbbe57f8f6d2b1e02a
Bipin B Narayan <bbnaraya@redhat.com>
2026-03-17 16:27:05 +0530

    dracut-modules: omit in kdump

--- dracut/01ignition-scsi-rules/module-setup.sh
+++ dracut/01ignition-scsi-rules/module-setup.sh
@@ -1,6 +1,12 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
+check() {
+    if [[ $IN_KDUMP == 1 ]]; then
+        return 1
+    fi
+}
+
 # 1) Fix for https://bugzilla.redhat.com/show_bug.cgi?id=1918244
 # On s390x systems with IBM 2810XIV discs multipath couldn't be configured
 # because SCSI_IDENT_* udev properties are not set at boot time
